7047de87c7c00ef2468335923c424398a861c082
Joe Good <joe.d.good@gmail.com>
2015-09-30 23:19:42 -0500

    Changed:  Renamed the project file to include "-console", in case I added different project types to the solution. Added:  ImageLib class is starting...

--- Program.cs
+++ Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Piconator.Classes;
 
-namespace piconator
+namespace Piconator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Set up the size of the console window.  Curious, what does this do under Linux?
+
+            double ratio = 0.9;
+            Console.SetWindowSize((int)Math.Round(Console.LargestWindowWidth * ratio), (int)Math.Round(Console.LargestWindowHeight * ratio));
+            Console.SetBufferSize(Console.WindowWidth, 10000);
+
+            Console.CursorSize = 5;
+
+            Console.WriteLine("CursorLeft = {0}   CursorSize = {1}  CursorTop={2}  CursorVisible={3}", Console.CursorLeft, Console.CursorSize, Console.CursorTop, Console.CursorVisible);
+
+            var gInfo = ImageLib.AnalyzeV1("anim/matrix.gif");
+            foreach( string line in gInfo)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("");
+
+            gInfo = ImageLib.AnalyzeV1("stills/pacman-blinky.png");
+            foreach ( string line in gInfo)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("");
+
+
+
+            Console.WriteLine("Press any key to start anim...");
+            Console.ReadKey();
+
+            int counter;
+            string[] sequence;
+
+            counter = 0;
+            sequence = new string[] { "/", "-", "\\", "|" };
+            sequence = new string[] { ".", "o", "0", "o" };
+            sequence = new string[] { "+", "x" };
+            sequence = new string[] { "V", "<", "^", ">" };
+            //sequence = new string[] { ".   ", "..  ", "... ", "...." };
+
+            for (int n = 0; n <= 10000; n++)
+            {
+                counter++;
+
+                if (counter >= sequence.Length)
+                    counter = 0;
+
+                Console.Write(sequence[counter]);
+                Console.SetCursorPosition(Console.CursorLeft - sequence[counter].Length, Console.CursorTop);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
         }
     }
 }
---
+++ classes/ImageLib.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Piconator.Classes
+{
+    class ImageLib
+    {
+
+        //http://www.vcskicks.com/csharp_animated_gif2.php
+        // possibly use this:  https://magick.codeplex.com/
+
+
+        public static List<String> AnalyzeV1(string gifPath)
+        {
+            var result = new List<String>();
+
+            // Analyzes gif info System.Drawing.Imaging
+
+            result.Add(String.Format("Full Path = {0}", System.IO.Path.GetFullPath(gifPath)));
+
+            Image gifImage = Image.FromFile(gifPath);
+            
+
+            FrameDimension dimension = new FrameDimension(gifImage.FrameDimensionsList[0]);
+            int frameCount = gifImage.GetFrameCount(dimension);
+
+            result.Add(String.Format("FrameCount = {0}", frameCount));
+
+            var flags = (ImageFlags)gifImage.Flags;
+
+            if (flags.HasFlag(ImageFlags.Scalable)) result.Add("Flag.Scalable");
+            if (flags.HasFlag(ImageFlags.HasAlpha)) result.Add("Flag.HasAlpha");
+            if (flags.HasFlag(ImageFlags.HasTranslucent)) result.Add("Flag.HasTranslucent");
+            if (flags.HasFlag(ImageFlags.PartiallyScalable)) result.Add("Flag.PartiallyScalable");
+            if (flags.HasFlag(ImageFlags.ColorSpaceRgb)) result.Add("Flag.ColorSpaceRgb");
+            if (flags.HasFlag(ImageFlags.ColorSpaceCmyk)) result.Add("Flag.ColorSpaceCmyk");
+            if (flags.HasFlag(ImageFlags.ColorSpaceGray)) result.Add("Flag.ColorSpaceGray");
+            if (flags.HasFlag(ImageFlags.ColorSpaceYcbcr)) result.Add("Flag.ColorSpaceYcbcr");
+            if (flags.HasFlag(ImageFlags.ColorSpaceYcck)) result.Add("Flag.ColorSpaceYcck");
+            if (flags.HasFlag(ImageFlags.HasRealDpi)) result.Add("Flag.HasRealDpi");
+            if (flags.HasFlag(ImageFlags.HasRealPixelSize)) result.Add("Flag.HasRealPixelSize");
+            if (flags.HasFlag(ImageFlags.ReadOnly)) result.Add("Flag.ReadOnly");
+            if (flags.HasFlag(ImageFlags.Caching)) result.Add("Flag.Caching");
+
+            result.Add(String.Format("Height = {0}", gifImage.Height));
+            result.Add(String.Format("Width = {0}", gifImage.Width));
+            result.Add(String.Format("HorizontalResolution = {0}", gifImage.HorizontalResolution));
+            result.Add(String.Format("VerticalResolution = {0}", gifImage.VerticalResolution));
+
+            result.Add(String.Format("PixelFormat = {0}", gifImage.PixelFormat));
+
+            return result;
+
+        }
+    }
+
+
+}
